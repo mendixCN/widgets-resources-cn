d43612b45a6e74528314a440da94c13937104e28
engalar <engalar@gmail.com>
2021-08-10 17:49:21 +0800

    百度图

--- packages/pluggableWidgets/AMap/src/components/HelloWorldSample.tsx
+++ packages/pluggableWidgets/AMap/src/components/HelloWorldSample.tsx
@@ -1,11 +1,37 @@
-import { Component, ReactNode, createElement } from "react";
+import { createElement } from "react";
+import { Map, APILoader, ScaleControl, ToolBarControl, ControlBarControl, Geolocation } from '@uiw/react-amap';
+
+import './AMapSDK';
 
 export interface HelloWorldSampleProps {
     sampleText?: string;
 }
 
-export class HelloWorldSample extends Component<HelloWorldSampleProps> {
-    render(): ReactNode {
-        return <div className="widget-hello-world">Hello {this.props.sampleText}</div>;
-    }
+const Demo = () => (
+
+        <Map>
+            <ScaleControl offset={[16, 30]} position="LB" />
+            <ToolBarControl offset={[16, 10]} position="RB" />
+            <ControlBarControl offset={[16, 180]} position="RB" />
+            <Geolocation
+                maximumAge={100000}
+                borderRadius="5px"
+                position="RB"
+                offset={[16, 80]}
+                zoomToAccuracy={true}
+                showCircle={true}
+            />
+        </Map>
+    
+);
+
+export const HelloWorldSample = (props: HelloWorldSampleProps) => {
+
+    console.log(props);
+
+    return <div style={{ width: '100%', height: 300 }}>
+        <APILoader>
+            <Demo></Demo>
+        </APILoader>
+    </div>
 }
