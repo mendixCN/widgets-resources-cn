50fee19df5491d2949440fcf79d1071323f13c97
engalar <engalar@gmail.com>
2021-08-15 06:20:14 +0000

    no issue

--- scripts/gitpod/build.sh
+++ scripts/gitpod/build.sh
@@ -12,12 +12,12 @@ cp -r $ROOTDIR/testProject/ $ROOTDIR/docker-mendix-buildpack/project
 cd $ROOTDIR/docker-mendix-buildpack
 
 #--build-arg BUILDER_ROOTFS_IMAGE=mendix/rootfs:bionic \
-export FORCED_MXRUNTIME_URL=https://cdn.mendix.com/runtime/mendix-9.4.0.24572.tar.gz
+#  --build-arg FORCED_MXRUNTIME_URL=https://cdn.mendix.com/runtime/mendix-9.4.0.24572.tar.gz \
+# export FORCED_MXRUNTIME_URL=https://cdn.mendix.com/runtime/mendix-9.4.0.24572.tar.gz
 docker build \
   --build-arg BUILD_PATH=project \
   --build-arg ROOTFS_IMAGE=mendix/rootfs:bionic \
-  --build-arg FORCED_MXRUNTIME_URL=https://cdn.mendix.com/runtime/mendix-9.4.0.24572.tar.gz \
   --build-arg CF_BUILDPACK=v4.17.1 \
-  --tag mendix/mendix-buildpack:v1.4 .
+  --tag mendix/mendix-buildpack:v1.5 .
 
-  # docker run --rm -it --entrypoint /bin/bash mendix/mendix-buildpack:v1.4 -c ls /opt/mendix/build/runtimes/9.4.0.24572/runtime/
+  # docker run --rm -it --entrypoint /bin/bash mendix/mendix-buildpack:v1.5 -c ls /opt/mendix/build/runtimes/9.4.0.24572/runtime/
