c7182c7367ad6f68b39c8ff1e5801b8d6137ee39
engalar <engalar@gmail.com>
2021-09-20 16:45:42 +0800

    fix(ant-table-web): isolate css conflict

--- packages/pluggableWidgets/ant-table-web/src/components/TableWrapper.tsx
+++ packages/pluggableWidgets/ant-table-web/src/components/TableWrapper.tsx
@@ -8,16 +8,20 @@ export interface TableWrapperProps extends TableComponentProps {
     enablePaging?: boolean;
 }
 export default function TableWrapper(props: TableWrapperProps) {
-    return props.enablePaging ? (
-        <TableComponent dataSource={props.dataSource} columns={props.columns}></TableComponent>
-    ) : (
-        <VirtualTable
-            columns={props.columns}
-            dataSource={props.dataSource}
-            scroll={{
-                y: 300,
-                x: "100vw"
-            }}
-        ></VirtualTable>
+    return (
+        <div className="ant-table-web">
+            {props.enablePaging ? (
+                <TableComponent dataSource={props.dataSource} columns={props.columns}></TableComponent>
+            ) : (
+                <VirtualTable
+                    columns={props.columns}
+                    dataSource={props.dataSource}
+                    scroll={{
+                        y: 300,
+                        x: "100vw"
+                    }}
+                ></VirtualTable>
+            )}
+        </div>
     );
 }
