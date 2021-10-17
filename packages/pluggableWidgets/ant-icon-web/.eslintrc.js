81ad46e7c0ca9760e83d4e4e30d527460bb5cddf
engalar <engalar@gmail.com>
2021-10-17 21:04:43 +0800

    fix(ant-icon-web): lint config

--- packages/pluggableWidgets/ant-icon-web/.eslintrc.js
+++ packages/pluggableWidgets/ant-icon-web/.eslintrc.js
@@ -1,5 +1,6 @@
 const base = require("@mendix/pluggable-widgets-tools/configs/eslint.ts.base.json");
 
 module.exports = {
-    ...base
+    ...base,
+    "ignorePatterns": ["src/components/font_8d5l8fzk5b87iudi.js"]
 };
